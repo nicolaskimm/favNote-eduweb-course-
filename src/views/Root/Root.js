e8256c8be4b8f385745de12209f35f29412ca35d
Joanna Krezolek <j.krezolek@gmail.com>
2019-05-15 21:42:24 +0200

    clean the project & add styled-components

---
+++ src/views/Root/Root.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Button from 'components/Button/Button';
+
+const Root = () => (
+  <div>
+    <h1>hello</h1>
+    <Button>hi hello</Button>
+  </div>
+);
+
+export default Root;
