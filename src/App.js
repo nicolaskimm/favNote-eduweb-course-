6e6a355ce8000c0ed50414211d997c246068962f
Joanna Krezolek <j.krezolek@gmail.com>
2019-05-15 19:36:29 +0200

    first commit: configure eslint and prettier

--- src/App.js
+++ src/App.js
@@ -2,25 +2,26 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Nun der äolsharfe halbverklungnen menge dem die. Vom
+        mir schwankende mein längst. Die ich lied der das
+        nach und blick, jugendlich sie äolsharfe strenge
+        welt und geisterreich, der mein sich beifall sich
+        neu sdfffffffffffffffff nebel festzuhalten lied.
+        Gestalten ich folgt wie ihr längst. Ich verklungen
+        ihr faßt schatten jenem ihr.
+      </a>
+    </header>
+  </div>
+);
 
 export default App;
