593df277b112ad6b179047ccf17241c897d57825
Joanna Krezolek <j.krezolek@gmail.com>
2019-05-16 16:00:26 +0200

    add storybook & first stories

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,7 +1,25 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 const Button = styled.button`
-  background-color: yellow;
+  padding: 0;
+  background-color: ${({ color }) => color || 'hsl(49, 100%, 58%)'};
+  width: 220px;
+  height: 47px;
+  border: none;
+  border-radius: 50px;
+  font-family: 'Montserrat';
+  font-size: 16px;
+  font-weight: 600;
+  text-transform: uppercase;
+
+  ${({ secondary }) =>
+    secondary &&
+    css`
+      background-color: hsl(0 0%, 90%);
+      width: 105px;
+      height: 30px;
+      font-size: 10px;
+    `}
 `;
 
 export default Button;
--- src/views/Root/Root.js
+++ src/views/Root/Root.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Button from 'components/Button/Button';
+import GlobalStyle from 'theme/GlobalStyle';
 
 const Root = () => (
   <div>
+    <GlobalStyle />
     <h1>hello</h1>
-    <Button>hi hello</Button>
+    <Button>Close / Save</Button>
+    <Button secondary>Remove</Button>
   </div>
 );
 
